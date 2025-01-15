38c9bdc04ccfb06cb9cc9c748e95050fd92166b8
Isaac Bevis <isaacbevis123@gmail.com>
2025-01-15 15:59:38 -0700

    Fix up contact page ref #3

---
+++ js/contact.js
@@ -0,0 +1,28 @@
+const button = document.getElementById("contactButton");
+
+button.addEventListener("click", function() {
+	window.open("mailto:thebevi@thecolobevi.com");
+});
+
+button.addEventListener("mouseover", function() {
+	/*
+	background-color: #4b83ce;
+	border-color: #6dabf4;
+	border-width: 0.2em;
+	margin-top: 0.1em;
+	margin-left: 3em;
+	*/
+	this.style.backgoundColor = "4B8AFF";
+	this.style.borderColor = "#6DAFFF";
+	this.style.borderWidth = "0.3em";
+	this.style.marginTop = "0em";
+	this.style.marginLeft = "2.8";
+});
+
+button.addEventListener("mouseout", function() {
+	this.style.backgoundColor = "";
+	this.style.borderColor = "";
+	this.style.borderWidth = "";
+	this.style.marginTop = "";
+	this.style.marginLeft = "";
+});
